ff249c19a7b39fc0e38b78e6472bbf42f707c91c
Nathan Edwards <edwardsnj@gmail.com>
2023-05-03 12:53:46 -0400

    Dummy up table display

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# exscsnv-catalog
-Expressed single-cell SNV catalog
+
+Introduction text,.........
+
+## Studies
+
+<div class="d3table" dataurl="data/study_catalog.tsv" colsurl="data/study_catalog.cols"/>
+
+## Samples
+
+<div class="d3table" dataurl="data/sample_catalog.tsv" colsurl="data/sample_catalog.cols"/>
+
---
+++ js/d3table.js
@@ -0,0 +1,78 @@
+
+"use strict";
+
+let d3table {
+    this.init() {
+        var elts = document.getElementsByClass('d3table');
+        for (var i=0; i<elts.length; i++) {
+            var elt = elts[i];
+            var dataurl = elt.getAttribute('dataurl');
+            var colsurl = elt.getAttribute('colsurl');
+            var t = new D3Table(elt, dataurl, colsurl);
+	}
+    }
+}
+
+function D3Table(elt,dataurl,colsurl) {
+    this.divid = elt.getAttribute('id');
+    this.tableid = this.divid + "-thetable";
+    this.dataurl = dataurl;
+    this.colsurl = colsurl;
+    this.thetable = null;
+    this.value = null;
+    if (this.elt) {
+        var datatype = dataurl.substring(dataurl.lastIndexOf(".")+1).toLowerCase();
+        var parser = d3.json;
+        if (datatype == "tsv") {
+            parser = d3.tsv;
+        } else if (datatype == "csv") {
+            parser = d3.csv;
+        }
+        dataurl = dataurl +"?random=" + Math.random();
+        colsurl = dataurl +"?random=" + Math.random();
+        parser(dataurl, function(data) {
+            d3.json(colsurl, function(columns) {
+                var table = elt.append("table")
+                               .attr("id",this.tableid)
+		               .attr("class","ExcelTable2007")
+		               .attr("style","table-layout:fixed;")
+                var thead = table.append("thead");
+                var tbody = table.append("tbody").attr("class","list");
+
+                thead.append("tr")
+                     .selectAll("th")
+                     .data(columns)
+                     .enter()
+                     .append("th")
+	             .attr("align","center")
+	             .attr("valign","bottom")
+	             .attr("width",function(column) {
+	                 if (column.width !== undefined) {
+	                     return column.width;
+                         }
+	                 return null;
+                     })
+	             .attr("class",function(column) {
+	                 if (column.oddeven !== undefined && column.oddeven) {
+	                     return "mysort oddeven";
+                         } 
+	                 return "mysort";
+                     })
+	             .attr("data-sort",function(column) { return column.tag; })
+	             .attr("data-type",function(column) { 
+                         if (column.type == "extid") {
+                             if (column.idtype == "int") {
+                               return "urlint"
+                             } else {
+                               return "url"
+                             }
+                         } else {
+                             return column.type;
+                         }
+                     })
+                     .text(function(column) { return column.label; });
+
+    }
+}
+D3Table.prototype = new D3Table();
+D3Table.prototype.constructor = D3Table;
