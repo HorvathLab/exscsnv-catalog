7424ef241c28c853b8466c373c42b81d9fa7b0fb
Nathan Edwards <edwardsnj@gmail.com>
2023-05-04 10:24:39 -0400

    finally working, basics

--- js/d3table.js
+++ js/d3table.js
@@ -11,8 +11,16 @@ let d3table = {
             var colsurl = elt.getAttribute('cols');
             var t = new D3Table(divid, dataurl, colsurl);
 	}
+    },
+    htmlescape: function(unsafe) {
+        return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;")
     }
-}
+};
 
 function D3Table(divid,dataurl,colsurl) {
     if (divid!==undefined) {
@@ -78,6 +86,58 @@ function D3Table(divid,dataurl,colsurl) {
                          }
                      })
                      .text(function(column) { return column.label; });
+
+                //
+                var rows = tbody.selectAll("tr")
+                                .data(data)
+                                .enter()
+                                .append("tr")
+                                .attr("class","rowshow");
+                //
+                var cells = rows.selectAll("td")
+                                .data(function(row,i) {
+                                    return columns.map(function(column) {
+		                        return {column: column, value: row[column.tag], row: row};
+                                    });
+                                })
+                                .enter()
+                                .append("td")
+	                        .attr("align",function(d) { 
+	                            if (d.column.align !== undefined) {
+	                                return d.column.align;
+                                    }
+	                            return "left";
+	                        })
+	                        .attr("valign","top")
+	                        .attr("class",function (d) {
+	                            return d.column.tag; 
+	                        })
+                                .html(function(d) { 
+	                            if (d.column.type == "float" && d.column.fixed !== undefined) {
+	                                return parseFloat(d.value).toFixed(d.column.fixed);
+                                    } else if (d.column.type == "extid" && d.column.exturl !== undefined) {
+	                                return "<A href="+d.column.exturl+d.value+" target=\"_blank\">"+d.value+"</A>";
+                                    } else if (d.column.type == "img" && d.column.imgurl !== undefined) {
+	                                url = d.column.imgurl.replace(/{}/g, d.value);
+		                        var height = ""
+		                        if (d.column.height !== undefined) {
+		                            height = " height=\""+d.column.height+"\"";
+		                        }
+		                        if (d.column.width !== undefined) {
+		                            width = " width=\""+d.column.width+"\"";
+		                        }
+		                        if (d.column.caption !== undefined) {
+		                            return "<P align=\"center\"><IMG style=\"margin: 10px;\" src=\""+url+"\""+height+width+" loading=\"lazy\" ><br/>"+d.value+"</P>";
+		                        }
+                                        if (d.column.exturl !== undefined) {
+                                            var exturl = d.column.exturl.replace(/{}/g, d.row[d.column.exturltag]);
+	                                    return "<A href=\""+exturl+"\" target=\"_blank\"><IMG src=\""+url+"\""+height+width+" loading=\"lazy\" ></A>";
+                                        }
+	                                return "<IMG src=\""+url+"\""+height+width+" loading=\"lazy\" >";
+                                   };
+	                           return d.value; 
+	                       });
+
             });
         });
     }
