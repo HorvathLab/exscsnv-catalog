a2b137cf9d4e00cbbaa4af757314d0229978a7fd
Nathan Edwards <edwardsnj@gmail.com>
2024-01-03 13:38:28 -0500

    update ST0004's samples

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,4 @@ fi
 ./scripts/make_catalog.py ./data > ./data/study_catalog.tsv
 ./scripts/make_samples.py ./data > ./data/sample_catalog.tsv
 ./scripts/make_study_json.py ./data
-./scripts/make_study_readme ./data
+./scripts/make_study_readme.py ./data
